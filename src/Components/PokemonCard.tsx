d7730f404d652d3f7db263f0a77694ea4ddfcb26
Nicolas GIRARD <NG38@outlook.fr>
2025-04-01 14:26:33 +0200

    components.2

--- src/Components/PokemonCard.tsx
+++ src/Components/PokemonCard.tsx
@@ -3,22 +3,20 @@ const pokemonList = [
         name: "Ronflex",
         imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/143.png"
     },
-    {   
-        name: "Mew"
+    {   name: "mew"
     }
-];
+]
 
-function PokemonCard() {
+function PokemonCard(){
+    const pokemon = pokemonList[1]
+
     return (
-        <div>
-            {pokemonList.map((pokemon, index) => (
-                <figure key={index}>
-                    {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
-                    {pokemon.name ? <figcaption>{pokemon.name}</figcaption> : <p>???</p>}
-                </figure>
-            ))}
-        </div>
-    );
+        <figure>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+            {pokemon.name ? <figcaption>{pokemon.name}</figcaption> : <p>???</p>}
+
+        </figure>
+    )
 }
 
 
